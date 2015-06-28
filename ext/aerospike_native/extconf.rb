bc272ee63aaa6c6ce09d4a5d57819151afa97671
Vladimir Ziablitskii <zyablitskiy@gmail.com>
2015-06-28 13:07:54 +0600

    add aerospike compile from source

--- ext/aerospike_native/extconf.rb
+++ ext/aerospike_native/extconf.rb
@@ -2,11 +2,30 @@ require 'mkmf'
 
 extension_name = 'aerospike_native'
 
+find_executable('make')
+find_executable('git')
+have_library('crypto')
+have_library('libc')
+have_library('openssl')
+
+headers_path = File.expand_path(File.join(File.dirname(__FILE__), "include"))
+lib_path = File.expand_path(File.join(File.dirname(__FILE__), "lib"))
+aerospike_client_c_dir = File.expand_path(File.join(File.dirname(__FILE__), "aerospike-client-c"))
+`git clone https://github.com/aerospike/aerospike-client-c.git`
+Dir.chdir(aerospike_client_c_dir) do
+  `git reset --hard f4aa41fc237fca3e25110d15e72b7735262e6653`
+  `git submodule update --init`
+  `make`
+  target_dir = Dir.glob("target/*").first
+  `cp -p #{target_dir}/lib/libaerospike.so #{lib_path}`
+  `cp -rp #{target_dir}/include/* #{headers_path}`
+end
+
 LIBDIR     = RbConfig::CONFIG['libdir']
 INCLUDEDIR = RbConfig::CONFIG['includedir']
 
-HEADER_DIRS = [INCLUDEDIR, File.expand_path(File.join(File.dirname(__FILE__), "include"))]
-LIB_DIRS = [LIBDIR, File.expand_path(File.join(File.dirname(__FILE__), "lib"))]
+HEADER_DIRS = [INCLUDEDIR, headers_path]
+LIB_DIRS = [LIBDIR, lib_path]
 
 libs = ['-laerospike', '-lcrypto']
 dir_config(extension_name, HEADER_DIRS, LIB_DIRS)
